fbbe2648b86b1a0350073f7f1e08d25bb223465c
Jerish PJ <jerish.pj@hotmail.com>
2025-07-14 16:14:56 +0530

    Added user and tasks components

--- TaskManager.WebUI/src/app/app.ts
+++ TaskManager.WebUI/src/app/app.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'TaskManager.WebUI';
+  users = DUMMY_USERS
+  selectedUserId = '';
+
+  get selectedUserName() {
+    const user = this.users.find(user => user.id === this.selectedUserId);
+    return user ? user.name : '';
+  }
+
+  onUserSelect(id: string) {
+    console.log(`AppComponent: User selected: ${id}`);
+    this.selectedUserId = id;
+  }
 }
--- TaskManager.WebUI/src/app/user/user.ts
+++ TaskManager.WebUI/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component,  EventEmitter,  Input, Output, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';  
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,15 +11,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  // select = output<string>() // This code can be used if you want to use the output decorator directly
+
+get imagePath(){
+    return 'assets/users/' + this.avatar;
+}
 
   onSelectUser() {
     // Logic to handle user selection can be added here
-    console.log(`Selected user: ${this.selectedUser().name}`);
-
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    console.log(`UserComponent: Selected user: ${this.name}`);
+    this.select.emit(this.id);
   }
 }
